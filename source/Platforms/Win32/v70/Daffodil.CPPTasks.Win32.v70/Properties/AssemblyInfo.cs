befd2769e2756a493fd01231405dff4ed04458e5
Owen Wengerd <owenw@manusoft.com>
2024-11-09 16:54:38 -0500

    Updated assembly versions and installer version to 2.6.5.

--- source/Platforms/Win32/v70/Daffodil.CPPTasks.Win32.v70/Properties/AssemblyInfo.cs
+++ source/Platforms/Win32/v70/Daffodil.CPPTasks.Win32.v70/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ManuSoft")]
 [assembly: AssemblyProduct("Daffodil.CPPTasks.Win32.v70")]
-[assembly: AssemblyCopyright("Copyright © 2013 ManuSoft")]
+[assembly: AssemblyCopyright("Copyright © 2024 ManuSoft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.2")]
-[assembly: AssemblyFileVersion("4.0.0.2")]
+[assembly: AssemblyVersion("4.0.0.3")]
+[assembly: AssemblyFileVersion("4.0.0.3")]
